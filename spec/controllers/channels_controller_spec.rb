fe0e582c3fae6d4ef78471286c6cf31b81f8a849
Joshua Retallick <josh@old-empire.co.uk>
2021-02-18 10:51:49 +0000

    Tests for restful routes when user visits /new & /index path

--- spec/controllers/channels_controller_spec.rb
+++ spec/controllers/channels_controller_spec.rb
@@ -20,4 +20,19 @@ RSpec.describe ChannelsController, type: :controller do
             # expect(response).to have_http_status(302) # Expects a HTTP Status code of 302
         end
     end
+
+    describe 'GET /new ' do
+      it 'responds with 200' do
+        get :new
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe 'GET /' do
+      it 'responds with 200' do
+        get :index
+        expect(response).to have_http_status(200)
+      end
+    end
+
 end
