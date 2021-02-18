b9af6591689a178a40b3272d2f67b199f8afc734
Joshua Retallick <josh@old-empire.co.uk>
2021-02-18 09:47:35 +0000

    Sorted routing so you can only create a new channel if logged in

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_02_17_135353) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "channels", force: :cascade do |t|
     t.string "name"
     t.string "picture"
