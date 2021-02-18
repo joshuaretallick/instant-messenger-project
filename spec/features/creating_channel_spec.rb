024f8454a221fc5a6dff77535b53f9e8a5739898
Joshua Retallick <josh@old-empire.co.uk>
2021-02-18 11:45:50 +0000

    Removed photo upload test

--- spec/features/creating_channel_spec.rb
+++ spec/features/creating_channel_spec.rb
@@ -13,11 +13,4 @@ feature 'channel creation' do
     expect(page).to have_content('I love chasing squirrels')
   end
 
-  it 'lets the user create a channel with a picture when signed in' do
-    sign_up
-    channel_photo
-    expect(page).to have_content('Channel was successfully created.')
-    expect(page).to have_content('I love chasing squirrels')
-    expect(page).to have_css("img[src*='Hooper.jpg']")
-  end
 end
