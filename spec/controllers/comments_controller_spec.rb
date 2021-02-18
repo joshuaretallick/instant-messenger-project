54fc66a986b8ffe35334bb10c55159f9c9cdeafe
Joshua Retallick <josh@old-empire.co.uk>
2021-02-18 10:53:37 +0000

    Testing restful routes on comments controller

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe CommentsController, type: :controller do
+
+  login_user
+
+  describe 'GET /new ' do
+    it 'responds with 200' do
+      get :new
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /' do
+    it 'responds with 200' do
+      get :index
+      expect(response).to have_http_status(200)
+    end
+  end
+end
